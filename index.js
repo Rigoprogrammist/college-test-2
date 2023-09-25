2ae71f3d517203a313b44680978f6d34a32a8097
Vladimir <vovagarmonshikov@mail.ru>
2023-09-25 18:11:42 +0300

    задания

--- index.js
+++ index.js
@@ -15,6 +15,61 @@ const content = fs.readFileSync(path.join(
 ), 'utf-8');
 
 // BEGIN
+
+export default function solution (content){
+
+  //step 1
+      const rows = content.trim().split("\n").slice(2).map(line => line.split('|'));
+      const datanormal = rows.map(line => line.slice(1,7));
+      
+      console.log(`Count: ${datanormal.length}`)
+  //step 2
+  
+  const galacticks = datanormal.map(row=>row[1].trim())
+  const set1 = new Set(galacticks);
+  const keys = [...set1].sort().join(', '); 
+  //const uniqGalacticks = _.uniq(galacticks);;
+  console.log(`Galaxies: ${keys}`);
+  
+  //step 3 11
+  const radiusSunlights = datanormal.map(row=>row[4])
+  let maxRadius = Math.max(...radiusSunlights)
+  let minRadius = Math.min(...radiusSunlights)
+  console.log(`Farest from Earth: ${maxRadius} light years, closest to Earth: ${minRadius} light years`)
+  
+  //step 4
+  
+  const obj = {}
+  const distonEarth =  datanormal.map(row=>`${row[0]}:${row[4]}:${row[1]}`)
+    obj.distonEarth = distonEarth;
+  let separator =  obj.distonEarth.map(cortej => cortej.split(':'))
+  
+  let min = Math.min(...separator.map(row=>row[1]))
+  
+  const findCentauri = (arr) => {
+      const result = [];
+   
+       for (let i = 0; i < arr.length; i += 1) {
+          if (arr[i][1] == min) {
+              result.push(arr[i])
+          }
+        
+      }
+     
+     let solutionstep3 =  result.flat().map(line => line.trim());
+     return solutionstep3
+  
+  }
+  findCentauri(separator)
+  
+  let findStarinGalaxy = findCentauri(separator)
+  let removed = findStarinGalaxy.splice(1, 1)
+  console.log(`Closest to Earth: ${findStarinGalaxy[0]} in ${findStarinGalaxy[1]} galaxy` )
+  
+  }
+  
+
+
 const collection = data.map((row) => row.split('|'));
 
 
@@ -81,4 +136,20 @@ const names =  data.map(row=>`${row[3]}:${[row[4]]}`)
   const arraynames = obj.names;
   let namestrim =  arraynames.map((word => word.trim()))
  
+
+
+  ## Памятка по **настройке личных аккаунтов студентов и оборудования для проведения экзамена.**
+
+  Перед началом экзамена зайдите в свой аккаунт на компьютере. Для того, чтобы скачать репозиторий и делать коммиты вам нужно настроить SSH ключ. Как это сделать:
+  
+  1. Проверьте, что у вас есть SSH-ключ введя в терминале команду: `ssh -T git@github.com`.
+  2. Если доступ закрыт, вам нужно будет сгенерировать SSH-ключ, прочитать его и установить в настройках GitHub:
+      - `ssh-keygen` - эта команда гененирует ssh ключ. Каждый раз при вопросе нажимайте ентер.
+      - `cat ~/.ssh/id_rsa.pub` - эта команда читает содержимое ssh ключа.
+      - Войдите в свой профиль на гитхабе. Зайдите в настройки профиля (клик по иконке профиля справа вверху, настройки), перейдите в раздел "SSH-ключи", нажмите New ssh key. Ведите имя ключа в поле сверху, а все прочитанное содержимое нового ключа в поле ниже. Затем нажмите кнопку добавить.
+  3. Снова проверьте команду `ssh -T git@github.com`. В случае успеха вы должны увидеть приветствие по имени аккаунта.
+  4. Склонируйте репозиторий по SSH(!), попробуйте сделать коммит и запушить. Гит попросит вас установить глобальное имя пользователя и почту. Воспользуйтесь командами, которые подсказывает терминал и напишите глобальное имя, которое является вашим никнеймом на гитхабе и почту без кавычек. Снова попробуйте запушить. Если пуш прошел, значит вы успешно настроили окружение.
+  5. Еще у вас может возникнуть ошибка, которая указывает на execFileSync. Это происходит потому, что метод не может получить доступ до файла. Чтобы исправить эту ошибку сделайте 2 вещи: 1. исправьте окончания строк с CRLF на LF (в vscode это маленький пункт внизу справа) 2. не используйте виндовс (можно пользоваться wsl в винде).
+
+
 // END
